38edfa813927297cb5dac380632b0110bd69365f
Diane Lo <Diane@Dianes-MacBook-Pro.local>
2015-09-17 13:51:55 -0700

    works with hard coded data

--- app/controllers/labels_controller.rb
+++ app/controllers/labels_controller.rb
@@ -11,30 +11,55 @@ class LabelsController < ApplicationController
 	end
 
 	def create
-		label = Label.new(to_label_params)
+		# label = Label.new(to_label_params)
+		@label = EasyPost::Shipment.create(
+			:to_address => {
+			  :name => 'Diane Lo',
+			  :street1 => '1240 California St.',
+			  :city => 'San Francisco',
+			  :state => 'CA',
+			  :zip => '94109',
+			  :country => 'US',
+			  :phone => '6504551746',
+			  :email => 'dianelo0228@gmail.com' 
+			},
+		    :from_address => {
+		      :name => 'EasyPost',
+		      :street1 => '118 2nd St',
+		      :city => 'San Francisco',
+		      :state => 'CA',
+		      :zip => '94105',
+		      :country => 'US',
+		      :phone => '4155559999',
+		      :email => 'support@easypost.com' 
+		    },
+		    :parcel => {
+		      :length => 20.2, 
+		      :width => 10.9,
+		      :height => 5,
+		      :weight => 65.9
+		    },
+		  )      
 
-		to_address = EasyPost::Address.create(to_label_params
+		@label.buy(
+		  :rate => @label.lowest_rate
 		)
 
-		from_address = EasyPost::Address.create(from_label_params
-		)
+		puts @label.postage_label.label_url
 
-		parcel = EasyPost::Parcel.create(parcel_params
-		)
+		# shipment.buy(
+		#   :rate => shipment.lowest_rate
+		# )
 
-		@shipment = EasyPost::Shipment.create(
-		  :to_address => to_address,
-		  :from_address => from_address,
-		  :parcel => parcel,
-		)
+		# puts shipment.postage_label.label_url
 
-		if @shipment.save
-			redirect_to labels_path
-			flash[:notice] = "Label created"
-		else 
-			redirect_to :back
-			flash[:error] = "Sorry something went wrong!"
-		end
+		# if shipment.save
+		# 	redirect_to labels_path
+		# 	flash[:notice] = "Label created"
+		# else 
+		# 	redirect_to :back
+		# 	flash[:error] = "Sorry something went wrong!"
+		# end
 		
 
 
@@ -49,19 +74,19 @@ class LabelsController < ApplicationController
 	  @label = Label.find(params[:id])
 	end
 
-	private
+	# private
 
-	def to_label_params
-	  params.require(:label).permit(:name, :street1, :street2, :city, :state, :zip, :country, :phone)
-	end 
+	# def to_label_params
+	#   params.require(:label).permit(:name, :street1, :street2, :city, :state, :zip, :country, :phone)
+	# end 
 
-	def from_label_params
-		params.require(:label).permit(:from_company, :from_street1, :from_street2, :from_city, :from_state, :from_zip, :from_phone)
-	end
+	# def from_label_params
+	# 	params.require(:label).permit(:from_company, :from_street1, :from_street2, :from_city, :from_state, :from_zip, :from_phone)
+	# end
 
-	def parcel_params
-		params.require(:label).permit(:width, :length, :height,
-			:weight)
-	end
+	# def parcel_params
+	# 	params.require(:label).permit(:width, :length, :height,
+	# 		:weight)
+	# end
 
 end
